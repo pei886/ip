ca8b64bdfde2f7a01e50963ae2de50299a937eab
pei886 <jingyapei@gmail.com>
2025-08-20 22:57:34 +0800

    added list

--- src/main/java/Momo.java
+++ src/main/java/Momo.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Momo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] list = new String[100];
+        int count = 0;
 
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Momo");
@@ -13,17 +15,29 @@ public class Momo {
         while(true) {
             input = sc.nextLine();
             if(input.equalsIgnoreCase("bye")) {
-                System.out.println("____________________________________________________________");
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("____________________________________________________________");
+                bye();
                 break;
-            } else {
+            } else if (input.equalsIgnoreCase("list")) {
                 System.out.println("____________________________________________________________");
-                System.out.println(input);
+                for (int i = 0; i < count; i++) {
+                    System.out.println((i+1) + ". " + list[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                list[count] = input;
+                count ++;
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + input);
                 System.out.println("____________________________________________________________");
             }
         }
         sc.close();
 
+    }
+
+    public static void bye() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
