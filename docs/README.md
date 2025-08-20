416133bee0f6dd8d73c23a1ac5c402fadc8eb6fa
pei886 <jingyapei@gmail.com>
2025-08-20 22:22:39 +0800

    Add greeting

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Momo User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Momo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Momo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Momo");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+
+    }
+}
