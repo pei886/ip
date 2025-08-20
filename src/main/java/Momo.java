8b2df50f47b3f8dd246f79c40409d19eb65895c0
pei886 <jingyapei@gmail.com>
2025-08-20 22:27:26 +0800

    added echo

--- src/main/java/Momo.java
+++ src/main/java/Momo.java
@@ -7,9 +7,23 @@ public class Momo {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Momo");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+
+        String input;
+        while(true) {
+            input = sc.nextLine();
+            if(input.equalsIgnoreCase("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(input);
+                System.out.println("____________________________________________________________");
+            }
+        }
+        sc.close();
 
     }
 }
