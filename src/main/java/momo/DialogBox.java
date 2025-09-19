1abd0fa0d4f972645e7ac10f59b81bb3bbc65ac1
jhshj886 <jingyapei@gmail.com>
2025-09-19 20:28:01 +0800

    Better Gui: Make profile pictures circular

--- src/main/java/momo/DialogBox.java
+++ src/main/java/momo/DialogBox.java
@@ -13,6 +13,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
 
 /**
  * Represents a dialog box consisting of an ImageView to represent the speaker's face
@@ -35,6 +36,8 @@ public class DialogBox extends HBox {
         }
 
         dialog.setText(text);
+        Circle clip = new Circle(50, 50, 50);
+        displayPicture.setClip(clip);
         displayPicture.setImage(img);
     }
 
